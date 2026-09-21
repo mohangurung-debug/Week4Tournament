290a9d67b041d0a58951ad865b4cc663d5dd8c31
mohangurung-debug <mohan.gurung@stu.ocsb.ca>
2026-09-21 10:39:23 -0400

    added first team

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 #############################################
-# Name: Your name
+# Name: Mohan Gurung
 # Class: ICS3C
 # Date: Friday Sept. 25
 # Project Name: Week4Tournament
@@ -8,3 +8,12 @@
 #############################################
 
 # THIS IS WHERE YOU CODE
+print("What is your team name?")
+team1 = input()
+print("How many wins does your team currently have?")
+teamp1 = int(input())
+print("how many losses does your team currently have?")
+teamp2 = int(input())
+print("how many draws does your team currently have?")
+team13 = int(input())
+print("your team has", teamp1*2+teamp2*1,"points")
